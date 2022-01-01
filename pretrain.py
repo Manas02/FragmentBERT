f3f7dab5f66e22e38e8e1a5968b19438f38292d8
Manas Mahale <36999652+Manas02@users.noreply.github.com>
2022-01-02 04:31:51 +0530

    Add files via upload

---
+++ pretrain.py
@@ -0,0 +1,28 @@
+from tqdm import tqdm
+from rdkit import Chem
+from rdkit.Chem import BRICS
+from rdkit import RDLogger           
+
+
+def mol_to_smiles(mol):
+        return Chem.MolToSmiles(mol, isomericSmiles=True)
+
+def mol_from_smiles(smi):
+    return Chem.MolFromSmiles(smi)
+
+dummy = Chem.MolFromSmiles('[*]')
+
+def strip_dummy_atoms(mol):
+    hydrogen = mol_from_smiles('[H]')
+    mols = Chem.ReplaceSubstructs(mol, dummy, hydrogen, replaceAll=True)
+    mol = Chem.RemoveHs(mols[0])
+    return mol
+
+with open('smiles/canonical_train.txt') as f:
+    data = [i.strip() for i in f.readlines()]
+
+RDLogger.DisableLog('rdApp.*') 
+
+with open('smiles/canonical_train_scaffold.txt', 'w') as f:
+    for i in tqdm(data):
+        f.write(' '.join([mol_to_smiles(strip_dummy_atoms(mol_from_smiles(j))) for j in list(BRICS.BRICSDecompose(mol_from_smiles(i)))]) + '\\n')
